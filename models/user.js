16943d800300946e7e1c45b0329706aee51e1bf3
annoying bastard <annoying.bastard@gmx.de>
2017-02-09 00:07:21 +0100

    user model

---
+++ models/user.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var UserSchema = Schema(
+  {
+      username: { type:String, required:true, max:16 },
+      password: { type: String, required:true, max:16 }
+  }
+);
+
+AuthorSchema
+.virtual('url')
+.get(function (){
+  return '/user/' + this._id;
+});
+
+module.exports = mongoose.model('User', UserSchema);
