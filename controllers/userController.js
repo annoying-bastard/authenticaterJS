fa6bf9775528368e178e18bb56abf9a62453a842
annoying bastard <annoying.bastard@gmx.de>
2017-02-09 14:28:45 +0100

    - beautification measures

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,26 +10,27 @@ exports.user_get_post = function (req, res, next) {
 
   // VALIDATION of req.body.username
   // i have a feeling this is not good practice at all.
+  // maybe export the validation into ../validations/login.js?
   req.checkBody('username', 'invalid username').isAlpha().isEmpty()
-
   var errors = req.validationErrors();
-  if (errors) {
-    res.render('login', { info: "username can only contain alphanumeric characters" })
-  } else {
-    User.findOne({ 'username': req.body.username }, 'username password', function(err, user){
-      if (err) {
-        return next(err);
-      } else if (!user) {
-        res.render('login', { info: "User not found." });
-      } else if (user.password === req.body.password){
-        res.render('login', { info: "While the server accepted your login, this doesn't benefit you in any way as of yet" });
-      } else if (user.password != req.body.password){
-        res.render('login', { info: "You provided the wrong Password" });
-      } else {
-        res.render('login', { info: "Apparently something went wrong, this is embarassing." });
-      }
-    })
-  }
+  if (errors) {  res.render('login', { info: "username can only contain alphanumeric characters" }) }
+
+  // if errors is true the response ends with an error message
+  // so i feel it's save to omit the else statement for clarity reasons
+  // VALID DATA so process:
+  User.findOne({ 'username': req.body.username }, 'username password', function(err, user){
+    if (err) {
+      return next(err);
+    } else if (!user) {
+      res.render('login', { info: "User not found." });
+    } else if (user.password === req.body.password){
+      res.render('login', { info: "While the server accepted your login, this doesn't benefit you in any way as of yet" });
+    } else if (user.password != req.body.password){
+      res.render('login', { info: "You provided the wrong Password" });
+    } else {
+      res.render('login', { info: "Apparently something unexpected went wrong, this is embarassing." });
+    }
+  })
 
 }
 
