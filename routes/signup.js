1d7e66aa66ddcc599bdd8adc69e88ebaeb7511ce
annoying bastard <annoying.bastard@gmx.de>
2017-02-10 03:47:06 +0100

    fixed template data misplacement bug

--- routes/signup.js
+++ routes/signup.js
@@ -2,12 +2,10 @@ var express = require('express');
 var router = express.Router();
 var user_controller = require('../controllers/userController');
 
-// GET signup
 router.get('/', function (req, res, next) {
-  res.render('signup');
+  res.render('signup', { info: {}});
 })
 
-// POST signup --> user controller
 router.post('/', user_controller.user_register_post);
 
 module.exports = router;
